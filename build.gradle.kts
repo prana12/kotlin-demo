1f2f6448d7fa940637b9d07509780240340a92fa
Prakash Rana <prana12@live.com>
2023-08-20 11:23:01 +0100

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+	jvmTarget = "17"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+	jvmTarget = "17"
+}
---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,77 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.bind.Bindable.listOf
+import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import java.util.Collections.emptyList
+
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageController(val service: MessageService) {
+	@GetMapping("/")
+	//fun index(@RequestParam("name") name: String) = "Hello, $name!"
+	/*fun index() = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+	)*/
+	fun index(): List<Message> = service.findMessages()
+
+	@GetMapping("/{id}")
+	fun index(@PathVariable id: String): List<Message> =
+		service.findMessageById(id)
+
+	@PostMapping("/")
+	fun post(@RequestBody message: Message) {
+		service.save(message)
+	}
+}
+
+interface MessageRepository : CrudRepository<Message, String>
+
+@Table("MESSAGES")
+data class Message(@Id var id: String?, val text: String)
+//data class Message(val id: String?, val text: String)
+
+@Service
+/*
+class MessageService(val db: JdbcTemplate) {
+	fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
+	fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
+	fun save(message: Message) {
+		val id = message.id ?: UUID.randomUUID().toString()
+		db.update("insert into messages values ( ?, ? )",
+			id, message.text)
+	}
+}*/
+class MessageService(val db: MessageRepository) {
+	fun findMessages(): List<Message> = db.findAll().toList()
+
+	fun findMessageById(id: String): List<Message> = db.findById(id).toList()
+
+	fun save(message: Message) {
+		db.save(message)
+	}
+
+	fun <T : Any> Optional<out T>.toList(): List<T> =
+		if (isPresent) listOf(get()) else emptyList()
+}
